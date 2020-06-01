a23072ee1bb7d1d0d68e04c90922a6e9f9a19b29
M6code <benjamynfrank101@gmail.com>
2020-06-01 12:17:26 +0100

    Show details about a movie when clicked

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -5,7 +5,35 @@ function MovieDetail(props) {
     console.log(props.movie);
     return (
         <div className="movie-res-home">
-            <h1>{props.movie.Title}</h1>
+            <div className='parent-container'>
+                <div className='poster-detail'><img src={props.movie.Poster} alt='poster' /></div>
+                <div className='details'>
+                    <p>Title: {props.movie.Title}</p>
+                    <p>Year: {props.movie.Year}</p>
+                    <p>Genre: {props.movie.Genre}</p>
+                    <p>Actors: {props.movie.Actors}</p>
+                    <p>Director: {props.movie.Director}</p>
+                    <p>Runtime: {props.movie.Runtime}</p>
+                    <p>Box Office: {props.movie.BoxOffice}</p>
+                    <p>Country: {props.movie.Country}</p>
+                    <p>Awards: {props.movie.Awards}</p>
+                    <hr/>
+                    <div>
+                        Ratings:
+                        <p>{props.movie.Ratings.Value}</p>
+                        {props.movie.Ratings.map((rating, index) =>{
+                            return(
+                                <div key={index} className='ratings'>
+                                    <p>{rating.Source} : {rating.Value}</p>
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                    <p>Type: {props.movie.Type}</p>
+                    <p>Plot: {props.movie.Plot}</p>
+                </div>
+            </div>
         </div>
     )
 }
